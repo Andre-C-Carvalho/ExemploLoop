0c8c74947d1950e43fe33ce63ff4f15a1e52774c
Andre-C-Carvalho <andrecarvalho1978@gmail.com>
2021-09-02 18:53:11 -0300

    Criação arquivo ExemploLoop.js

---
+++ ExemploLoop.js
@@ -0,0 +1,24 @@
+let quantidadeDeAlunos = 10
+for (let contador = 0; contador <= quantidadeDeAlunos; contador++) {
+    if (contador === 0) {
+        console.log("O número atual é ZERO")
+    } else if (contador % 2 === 0){
+        console.log("O número atual é: " + contador + " (PAR)")
+    } else {
+        console.log("O número atual é: " + contador + " (ÍMPAR)")
+    }
+}
+
+// let quantidadeDeAlunos = 10
+// let contador = 0
+// while (contador <= quantidadeDeAlunos){
+//     if (contador === 0) {
+//         console.log("O número atual é ZER0")
+//     } else if (contador % 2 === 0){
+//         console.log("O número atual é: " + contador + " (PAR)")
+//     } else {
+//         console.log("O número atual é: " + contador + " (ÍMPAR)" )
+//     }
+//     contador++
+// }
+
